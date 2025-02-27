3628cd9d200fcdefdabdc3ae7fc1287bbedd6389
JaoNooTK <tonnavy33@gmail.com>
2025-02-27 10:12:32 +0700

    Update book.py

--- book.py
+++ book.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://chanuntorn133:tonkoawzaza33@cluster0.skxt5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://chanuntorn133:00@cluster0.skxt5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
